28e2acecc59f54d23fdc75ad3a2fa804905304df
Jens-VandePerre <98736383+Jens-VandePerre@users.noreply.github.com>
2022-03-08 13:42:25 +0100

    UD

--- Loop read and quantify mzML.R
+++ Loop read and quantify mzML.R
@@ -14,25 +14,20 @@ library(tidyverse)
 library(fs)
 library(proxyC)
 
-
 wd <- setwd("~/Desktop/Read raw file/Data mzML")
-getwd() #the first 4 mzML files of CPTAC
-list.files(wd)
+getwd() 
+list.files(wd) #The first 10 mzML files of CPTAC
 # Loading in multiple mzML files
 (file_paths <- fs::dir_ls("~/Desktop/Read raw file/Data mzML"))
-file_paths #The first 4 mzML files of CPTAC
+file_paths #The first 10 mzML files paths of CPTAC
 mzML_4files <- list() #empty list
 for (i in seq_along(file_paths)) {
   mzML_4files[[i]] <- readMSData(file_paths [[i]],
                                    msLevel = 2, verbose = FALSE, mode = "onDisk")
 }
-file_names <- c("01CPTAC_COprospective_W_PNNL_20170123_B1S1_f10.mzML",
-"08CPTAC_COprospective_W_PNNL_20170123_B2S4_f10.mzML",
-"18CPTAC_COprospective_W_PNNL_20170123_B5S2_f07.mzML",
-"21CPTAC_COprospective_W_PNNL_20170123_B5S5_f08.mzML")
 file_names_wd <- list.files(wd)
 file_names_wd
-mzML_4files2 <- set_names(mzML_4files, file_names_wd) #names each file by file_names
+mzML_4files2 <- set_names(mzML_4files, file_names_wd) #names each file by file_names_wd
 mzML_4files2
 
 #Loop extracting TMT intensities + Printing TMT intensities
@@ -52,7 +47,7 @@ for (i in seq_along(mzML_4files2)) {
   for (j in seq_along(mzML_4files_qnt)) {
     TMT_intensities[[j]] <- head(exprs(mzML_4files_qnt[[j]]),n=10) #only 10 first spectra
 }
-TMT_intensities2 <- set_names(TMT_intensities, file_names) #names each file by file_names
+TMT_intensities2 <- set_names(TMT_intensities, file_names_wd) #names each file by file_names_wd
 TMT_intensities2
 
     #Loop that outputs intensities for all spectra
@@ -74,12 +69,12 @@ for (i in seq_along(mzML_4files2)) {
     #all spectra, intensities for each TMT
   }
 }
-TMT_intensities3 <- set_names(TMT_intensities_2, file_names) #names each file by file_names
+TMT_intensities3 <- set_names(TMT_intensities_2, file_names_wd) #names each file by file_names_wd
 TMT_intensities3 #The Terminal output is unclear
 
-#####
+########
 #Extra's
-#####
+########
 # Loading in multiple mzML files
    #Method 2: purrr map
       #Automatically names the files
@@ -102,14 +97,13 @@ for (i in seq_along(mzML_4files)) {
     normalise("max")
 }
 data_names_qnt <- c("mzML.qnt1", "mzML.qnt2", "mzML.qnt3", "mzML.qnt4")    
-mzML_4files_qnt2 <- set_names(mzML_4files_qnt, file_paths) #names each file
+mzML_4files_qnt2 <- set_names(mzML_4files_qnt, file_names_wd) #names each file
 mzML_4files_qnt2
     #Printing TMT intensities
 intensities <- list() #empty list
 for (i in seq_along(mzML_4files_qnt)) {
   intensities[[i]] <-
   head(exprs(mzML_4files_qnt[[i]]))
-}
-data_names_qnt <- c("mzML.qnt1", "mzML.qnt2", "mzML.qnt3", "mzML.qnt4")    
-intensities2 <- set_names(intensities, data_names_qnt) #names each file
+} 
+intensities2 <- set_names(intensities, file_names_wd) #names each file
 intensities2
