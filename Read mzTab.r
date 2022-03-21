337a2677e4c4032ec2751121809f819c65601851
Jens-VandePerre <Jens.VandePerre@student.uantwerpen.be>
2022-03-21 15:48:56 +0100

    wip 21/3/22

--- Read mzTab.r
+++ Read mzTab.r
@@ -36,7 +36,7 @@ readMzTab <- function(filename) {
                                   sep="\t", na.strings="null", quote = "")
   mztab.table
 }
-(B1S2 <- readMzTab("02CPTAC_COprospective_W_PNNL_20170123_B1S2_f10.mztab")) #The first file
+(Test <- readMzTab("02CPTAC_COprospective_W_PNNL_20170123_B1S2_f10.mztab")) #The first file
 
 #Loop Reading in Files
   #File paths to direct the loop
@@ -48,7 +48,6 @@ file_names_short_typed <- c("B1S2_f10","B1S3_f05","B1S3_f10","B1S4_f02","B1S4_f0
     #Automate filename extraction
 file_names_wd #Long file names
 file_names_short <- substring(file_names_wd, 39, 46) #Character 39 untill 46 are unique
-
   #Loop reading mzTabs
 mzTab <- list() #empty list
 for (i in seq_along(file_paths)) {
@@ -56,11 +55,36 @@ for (i in seq_along(file_paths)) {
 }
 mzTab_files <- set_names(mzTab, file_names_short) #names each file by file_names_short
 mzTab_files
-
+  #View first file 
 view(mzTab_files[[1]])
-mzTab_files[[1]] %>% as_tibble()
-
+view(mzTab_files[[1]] %>% as_tibble())
 
  #Save mzMLs to different location: TMT outputs
 saveRDS(mzTab_files, file = "~/Desktop/mzTab/Stored files/Test 6 mzTabs")
 Test_6_mzTabs <- readRDS(file = "~/Desktop/mzTab/Stored files/Test 6 mzTabs")
+
+##################
+#Extract functions
+##################
+
+#extractMetadata
+extractMetadata <- function(mztab.table) {
+  mztab.table[startsWith(as.character(mztab.table$V1), "MTD"),]
+}
+?startsWith
+(mzTab_files_Metadata <- extractMetadata(mzTab_files[[1]]))
+
+#extracts columns V1:V3
+extractMetadata.V1V2V3 <- function(mztab.table) {
+  mztab.table[startsWith(as.character(mztab.table$V1), "MTD"), 1:3]
+}
+(mzTab_files_Metadata_V1V2V3 <- extractMetadata.V1V2V3(mzTab_files))
+
+#extractFeaturesPSM 
+extractFeaturesPSM <- function(mztab.table) {
+  psm <- mztab.table[startsWith(as.character(mztab.table$V1), "PSM"),]
+  prt <- mztab.table[startsWith(as.character(mztab.table$V1), "PRT"),]
+  rbind(psm,prt)
+}
+  #PSM or PRT present in datasets
+(mzTab_files_PSM <- extractFeaturesPSM(mzTab_files))
