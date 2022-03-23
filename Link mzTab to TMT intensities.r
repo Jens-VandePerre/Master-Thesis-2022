b316c407f9e5f5908f3057d91f031b005766740f
Jens-VandePerre <Jens.VandePerre@student.uantwerpen.be>
2022-03-23 16:35:17 +0100

    wip 23/3/22

--- Link mzTab to TMT intensities.r
+++ Link mzTab to TMT intensities.r
@@ -22,3 +22,13 @@ library("remotes")
 library("janitor")
 library("stringr")
 
+#Load 6 mzTabs
+mzTab <- readRDS(file = "~/Desktop/mzTab/Stored files/Test 6 mzTabs")
+
+
+#Load 6 matching mzMLs
+
+#Extract TMT intensities from these 6 mzMLs
+
+#Store this output
+saveRDS(file = "~/Desktop/mzTab/Stored files/6 matched mzMLS")
