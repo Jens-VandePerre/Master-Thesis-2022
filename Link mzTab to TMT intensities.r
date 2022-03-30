659bc907b4482dd6abf8b1eee962c0f72ae70efb
Jens-VandePerre <Jens.VandePerre@student.uantwerpen.be>
2022-03-30 10:13:21 +0200

    wip 30/3/22

--- Link mzTab to TMT intensities.r
+++ Link mzTab to TMT intensities.r
@@ -114,3 +114,13 @@ for (i in seq_along(TMT_ready_for_machting)) {
 Merged_PSM_TMT <- set_names(merging, file_names_short)
 Merged_PSM_TMT
 view(Merged_PSM_TMT[[1]])
+
+
+
+#Loading in matching mzMLs to mzTabs in wd
+getwd()
+list.files(wd)
+file_names_short
+
+list.files(path = "~/Desktop/Read raw file/Data mzML")
+list.files(path = "~/Desktop/ALL mzML")
