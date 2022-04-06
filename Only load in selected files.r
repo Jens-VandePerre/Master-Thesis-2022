cc8998e4f96352b5bfe4bedfa174288ebdb82c22
Jens-VandePerre <Jens.VandePerre@student.uantwerpen.be>
2022-04-06 16:10:44 +0200

    6/4/22 addapted for more mzMl and mzTab files

--- Only load in selected files.r
+++ Only load in selected files.r
@@ -21,21 +21,22 @@ library("msdata")
 library("remotes")
 library("janitor")
 library("stringr")
+library("purrr")
+
 
 #Working directory with all the wanted files
     #This wd has to contain all the files that have to be analyzed
-mzML_WD <- setwd("~/Desktop/mzTab/mzML corresponding to mzTab")
+mzTab_WD <- setwd("~/Desktop/Inputs/ALL_mzML")
 getwd()
-(file_names_wd <- list.files(mzML_WD)) #6 mzML files
+(file_names_wd <- list.files(mzTab_WD)) #6 mzML files
     #The wanted files
-file_names_short <- substring(file_names_wd, 39, 46) #Character 39 untill 46 are unique
-file_names_short #6 file names, both mzML and mzTab loaded
+(file_names_short <- substring(fs::dir_ls("~/Desktop/Inputs/ALL_mzTab"), 86, 93)) #Character 86 untill 93 are unique
 
 #Wanted mzMLs
     #Listing based on file_names_short
 listed_mzMLs <- list()
 for (i in seq_along(file_names_short)) {
-  listed_mzMLs[[i]] <- dir(path = "~/Desktop/mzTab/mzML corresponding to mzTab", pattern = file_names_short[[i]])
+  listed_mzMLs[[i]] <- dir(path = "~/Desktop/Inputs/ALL_mzML", pattern = file_names_short[[i]])
 }
 listed_mzMLs #from 16 mzML, the 6 from file_names_short are selected
     #Reading in listed mzMLs
@@ -43,19 +44,23 @@ mzML_files <- list() #empty list
 for (i in seq_along(listed_mzMLs)) {
   mzML_files[[i]] <- readMSData(listed_mzMLs[[i]], msLevel = 2, verbose = FALSE, mode = "onDisk")
 }
-Selected_mzML <- set_names(mzML_files, file_names_short)
-Selected_mzML
+(Selected_mzML <- set_names(mzML_files, file_names_short))
+ 
 
 #Wanted mzTabs
-listed_mzTabs <- list()
+sel_mzTabs <- list()
 for (i in seq_along(file_names_short)) {
-  listed_mzTabs[[i]] <- dir(path = "~/Desktop/mzTab/Imported mzTab", pattern = file_names_short[[i]])
+  sel_mzTabs[[i]] <- dir(path = "~/Desktop/Inputs/ALL_mzTab", pattern = file_names_short[[i]])
 }
-listed_mzTabs #from 23 mzTab, the 6 from file_names_short are selected
-    #Reading in listed mzTabs
+sel_mzTabs
+
+seleceted_mzTabs <- list[!sapply(list, identical, character(0))]
+ 
+ 
+    #Reading in selected_mzTabs
 mzTab_files <- list() #empty list
-for (i in seq_along(listed_mzTabs)) {
-  mzTab[[i]] <- readMzTab(listed_mzTabs[[i]])
+for (i in seq_along(seleceted_mzTabs)) {
+  mzTab[[i]] <- readMzTab(seleceted_mzTabs[[i]])
 }
 Selected_mzTab <- set_names(mzML_files, file_names_short)
 view(Selected_mzTab[[1]])
