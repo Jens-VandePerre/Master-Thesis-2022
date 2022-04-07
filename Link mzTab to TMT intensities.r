d2df9d29478b8048838046ce50d8f06985f050ee
Jens-VandePerre <Jens.VandePerre@student.uantwerpen.be>
2022-04-07 14:14:26 +0200

    7/4/22 Ran on 23 mzTabs an mzMLs

--- Link mzTab to TMT intensities.r
+++ Link mzTab to TMT intensities.r
@@ -22,63 +22,36 @@ library("remotes")
 library("janitor")
 library("stringr")
 
-#Load 6 PSMs
-PSM_6 <- readRDS(file = "~/Desktop/mzTab/Stored files/PSM column no modifications v2") 
-view(PSM_6[[1]]) 
-
-#Load 6 matching mzMLs
-wd <- setwd("~/Desktop/mzTab/mzML corresponding to mzTab")
+wd <- setwd("~/Desktop/Inputs/ALL_mzTab")
 getwd() 
-(file_names_wd <- list.files(wd)) #The first 6 mzML files
-(file_names_short <- substring(file_names_wd, 39, 46)) #Character 39 untill 46 are unique
-(file_paths <- fs::dir_ls("~/Desktop/mzTab/mzML corresponding to mzTab"))
-mzML <- list() #empty list
-for (i in seq_along(file_paths)) {
-  mzML[[i]] <- readMSData(file_paths[[i]],
-                         msLevel = 2, verbose = FALSE, mode = "onDisk")
-}
-mzML <- set_names(mzML, file_names_wd) #names each file by file_names_wd
-view(mzML)
-#Extract TMT intensities from these 6 mzMLs
-    #impute: method="MLE"
-    #no normalisation
-mzML_qnt <- list() #empty list
-TMT <- list() #empty list
-tbl <- list()
-for (i in seq_along(mzML)) {
-  mzML_qnt[[i]] <- 
-    quantify(mzML[[i]], method = "max", #max is the only working method
-             reporters = TMT10,
-             strict = FALSE,
-             verbose = FALSE) %>%
-    impute(method="MLE") %>%
-    purityCorrect(makeImpuritiesMatrix(10, edit = FALSE))
-  for (j in seq_along(mzML_qnt)) {
-    TMT[[j]] <- exprs(mzML_qnt[[j]]) #output all spectra, unclear in terminal
-  }
-}
-Matched_mzML_6 <- set_names(TMT, file_names_wd) #names each file by file_names_wd
-Matched_mzML_6
-    #Store this output
-saveRDS(Matched_mzML_6, file = "~/Desktop/mzTab/Stored files/6 matched mzMLS")
-TMT_Matched_mzML_6 <- readRDS(file = "~/Desktop/mzTab/Stored files/6 matched mzMLS")
+list.files(wd) #all mzTabs as of now 
+
+#Load PSMs 
+PSM_07_04_22 <- readRDS(file = "~/Desktop/Outputs/PSMs/07_04_22_PSMs") #PSMs stored 07/04/22
+view(PSM_07_04_22[[1]]) 
+#Load matching mzMLs
+TMT_Intensities_06_04_22 <- readRDS(file = "~/Desktop/Outputs/TMTs/06.04.22_TMT") #TMT intensities stored 06/04/22
+    #Automate filename extraction
+(file_name_long <- list.files(wd))
+(file_paths <- fs::dir_ls("~/Desktop/Inputs/ALL_mzTab"))
+(file_names_short <- substring(file_paths, 86, 93)) #Characters 86 untill 93 are uniqueue
 
 #Look for matching scan numbers
-view(PSM_6[[1]]["PSM_ID"]) #Column PSM_ID
-view(TMT_Matched_mzML_6[[1]][, 0]) #These are row names
+view(PSM_07_04_22[[1]]["PSM_ID"]) #Column PSM_ID
+view(TMT_Intensities_06_04_22[[1]][, 0]) #These are row names
 
 #Make TMT tibble + add index column for matching
   #selecting index column
 ind_TMT1 <- list()
-for (i in seq_along(TMT_Matched_mzML_6)) {
-  ind_TMT1[[i]] <- tibble(index=rownames(TMT_Matched_mzML_6[[i]][, 0])) %>%
-  mutate(index = trimws(str_remove_all(index, "F1.S"))) %>%
-  mutate(index = trimws(str_remove_all(index, "^0"))) %>%
-  mutate(index = trimws(str_remove_all(index, "^0"))) %>%
-  mutate(index = trimws(str_remove_all(index, "^0"))) %>%
-  mutate(index = trimws(str_remove_all(index, "^0"))) %>%
-  select(index) %>%
-  cbind(TMT_Matched_mzML_6[[i]]) %>% as_tibble
+for (i in seq_along(TMT_Intensities_06_04_22)) {
+  ind_TMT1[[i]] <- tibble(index=rownames(TMT_Intensities_06_04_22[[i]][, 0])) %>%
+    mutate(index = trimws(str_remove_all(index, "F1.S"))) %>%
+    mutate(index = trimws(str_remove_all(index, "^0"))) %>%
+    mutate(index = trimws(str_remove_all(index, "^0"))) %>%
+    mutate(index = trimws(str_remove_all(index, "^0"))) %>%
+    mutate(index = trimws(str_remove_all(index, "^0"))) %>%
+    select(index) %>%
+    cbind(TMT_Intensities_06_04_22[[i]]) %>% as_tibble
 }
 TMT_ready_for_machting <- set_names(ind_TMT1, file_names_short)
 TMT_ready_for_machting[[1]]
@@ -87,37 +60,37 @@ view(TMT_ready_for_machting[[1]])
 #Make PSM index column for matching
   #Extract numbers from PSM_ID column + Adding this index column to PSM_6
 ind_mzTab5 <- list()
-for (i in seq_along(PSM_6)) {
-ind_mzTab5[[i]] <- select(PSM_6[[i]], PSM_ID) %>% 
-                mutate(index = trimws(str_remove_all(PSM_ID, "controllerType=0 "))) %>%
-                mutate(index = trimws(str_remove_all(index, "controllerNumber=1 "))) %>%
-                mutate(index = trimws(str_remove_all(index, "scan="))) %>%
-                select(index) %>%
-                cbind(PSM_6[[i]]) %>% as_tibble
+for (i in seq_along(PSM_07_04_22)) {
+  ind_mzTab5[[i]] <- select(PSM_07_04_22[[i]], PSM_ID) %>% 
+    mutate(index = trimws(str_remove_all(PSM_ID, "controllerType=0 "))) %>%
+    mutate(index = trimws(str_remove_all(index, "controllerNumber=1 "))) %>%
+    mutate(index = trimws(str_remove_all(index, "scan="))) %>%
+    select(index) %>%
+    cbind(PSM_07_04_22[[i]]) %>% as_tibble
 }
 PSM_ready_for_matching <- set_names(ind_mzTab5, file_names_short)
 PSM_ready_for_matching
-view(PSM_ready_for_matching[[1]])
+view(PSM_ready_for_matching[[1]]) #PSM column could be removed
 
 #Merging by index
 merging <- list()
 for (i in seq_along(TMT_ready_for_machting)) {
   merging[[i]] <- merge(PSM_ready_for_matching[[i]], TMT_ready_for_machting[[i]], by="index") %>% 
-  as_tibble %>% arrange(desc(index[[i]]))
+  as_tibble
 }
 Merged_PSM_TMT <- set_names(merging, file_names_short)
 Merged_PSM_TMT
-view(Merged_PSM_TMT[[1]])
-view(Merged_PSM_TMT[[1]] %>% select(index, sequence_no_mod, 25:34))
+view(Merged_PSM_TMT[[1]]) 
+view(merging[[1]] %>% select(index, sequence_no_mod, 25:34) %>% arrange(sequence_no_mod)) 
   #Save outputs
-saveRDS(Merged_PSM_TMT, file = "~/Desktop/mzTab/Stored files/PSMs linked to TMT intensities")
-PSM_TMT <- readRDS("~/Desktop/mzTab/Stored files/PSMs linked to TMT intensities")
+saveRDS(Merged_PSM_TMT, file = "~/Desktop/Outputs/PSM_TMT_linked/07_04_22_PSM_TMT_Linked")
+PSM_TMT_07_04_22 <- readRDS("~/Desktop/Outputs/PSM_TMT_linked/07_04_22_PSM_TMT_Linked")
 
 #Checking if length stay the same after matching PSMs and TMTs
   #Length PSM
 l_PSM <- list()
-for (i in seq_along(PSM_6)) {
-  l_PSM[[i]] <- nrow(PSM_6[[i]])
+for (i in seq_along(PSM_TMT_07_04_22)) {
+  l_PSM[[i]] <- nrow(PSM_TMT_07_04_22[[i]])
 }
 (PSM_length <- set_names(l_PSM, file_names_short))
   #Length matched
@@ -128,17 +101,17 @@ for (i in seq_along(Merged_PSM_TMT)) {
 (Merged_length <- set_names(l_matched, file_names_short)) 
   #Is there a difference?
 l_diff <- list()
-for (i in seq_along(PSM_6)) {
+for (i in seq_along(PSM_TMT_07_04_22)) {
   l_diff[[i]] <- (PSM_length[[i]]-Merged_length[[i]])
 }
-(Length_difference <- set_names(l_diff, file_names_short)) #All lengts are the same. Merging SUCCESS!!!
+(Length_difference <- set_names(l_diff, file_names_short))#All 0 Merging SUCCESS
 
 #Selecting the collumn for relative quantification
 selected <- list()
 for (i in seq_along(Merged_PSM_TMT)) {
   selected[[i]] <- Merged_PSM_TMT[[i]] %>% 
   select("sequence_no_mod", "126":"131") %>%
-  rename(Peptide_sequence=sequence_no_mod, 
+  rename(Peptide_sequence = sequence_no_mod, 
         `Repoter intensity corrected 126` = `126`,
         `Repoter intensity corrected 127N` = `127N`,
         `Repoter intensity corrected 127C` = `127C`,
