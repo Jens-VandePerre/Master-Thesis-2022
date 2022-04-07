5da08ce1df3bbf0bd42d2a7f661c1410cbf1f163
Jens-VandePerre <Jens.VandePerre@student.uantwerpen.be>
2022-04-07 15:56:43 +0200

    7/4/22 Ran on 23 mzTabs + outputs saved to file

--- PSMs matching over files.r
+++ PSMs matching over files.r
@@ -22,9 +22,62 @@ library("remotes")
 library("janitor")
 library("stringr")
 
+#07/04/22 PSMs linked to TMT intensities for 23 files
+PSM_TMT_07_04_22 <- readRDS("~/Desktop/Outputs/PSM_TMT_linked/07_04_22_PSM_TMT_Linked")
+    #Make an extra index column that includes the file_names_short
+new_col <- list()
+for (i in seq_along(PSM_TMT_07_04_22)) {
+    new_col[[i]] <- PSM_TMT_07_04_22[[i]] %>%
+    mutate(index_filename = paste0(index, " ",file_names_short[[i]]), .before = index)
+}
+view(new_col[[4]])
+    #Selecting the sequence column + index_filename
+seq <- list()
+for (i in seq_along(new_col)) {
+    seq[[i]] <- select(new_col[[i]], index_filename, sequence)
+}
+(all_seq <- bind_rows(seq) %>% arrange(sequence, index_filename))
+view(all_seq) #all rows arrenged by sequence
+Sum_all_seq <- all_seq %>% add_column(count = rep(1, nrow(all_seq))) %>% 
+  group_by(sequence) %>%
+  summarise(Count = sum(count))
+view(Sum_all_seq) #sequence summarized, with a counter
+view(Sum_all_seq %>% arrange(-Count)) #most frequent sequence at top
+    
+    #Selecting the sequence_no_mod column + index_filename
+      #Different result, because the modification are not always the same
+seq_no_mod <- list()
+for (i in seq_along(new_col)) {
+    seq_no_mod[[i]] <- select(new_col[[i]], index_filename, sequence_no_mod)
+}
+(all_seq_no_mod <- bind_rows(seq_no_mod) %>% arrange(sequence_no_mod, index_filename))
+view(all_seq_no_mod) #all rows arrenged by sequence_no_mod
+Sum_all_seq_no_mod <- all_seq_no_mod %>%
+  add_column(count = rep(1, nrow(all_seq_no_mod))) %>%
+  group_by(sequence_no_mod) %>%
+  summarise(Count = sum(count))
+view(Sum_all_seq_no_mod) #sequence_no_mod summarized, with a counter
+view(Sum_all_seq_no_mod %>% arrange(-Count))#most frequent sequence_no_mod at top
+
+#Save the Outputs
+  #Sequence_no_mod
+saveRDS(all_seq_no_mod, file = "~/Desktop/Outputs/PSMs/07_04_22_Count_identical_seq_no_mod")
+Count_identical_seq_no_mod_07_04_22 <- readRDS(file = "~/Desktop/Outputs/PSMs/07_04_22_Count_identical_seq_no_mod")
+saveRDS(Sum_all_seq_no_mod, file = "~/Desktop/Outputs/PSMs/07_04_22_All_seq_no_mod")
+All_seq_no_mod_07_04_22 <- readRDS(file = "~/Desktop/Outputs/PSMs/07_04_22_All_seq_no_mod")
+  #Sequence
+saveRDS(Sum_all_seq, file = "~/Desktop/Outputs/PSMs/07_04_22_Count_identical_seq_with_mod")
+Count_identical_seq_with_mod_07_04_22 <- readRDS(file = "~/Desktop/Outputs/PSMs/07_04_22_Count_identical_seq_with_mod")
+saveRDS(all_seq, file = "~/Desktop/Outputs/PSMs/07_04_22_All_seq_with_mod")
+All_seq_with_mod_07_04_22 <- readRDS(file = "~/Desktop/Outputs/PSMs/07_04_22_All_seq_with_mod")
+
+
+######################
+#Extra: Starting from unchanged mzTab
+######################
 #Loading in PSMs
-    #01/04/22: Loading in the 23 mzTabs collected to date
-wd <- setwd("~/Desktop/mzTab/Imported mzTab")
+    #07/04/22: Loading in the 23 mzTabs collected to date
+wd <- setwd("~/Desktop/Outputs/mzTab/Imported mzTab")
 getwd() 
 list.files(wd) #23 mzTabs in this wd
     #file paths to 23 files
@@ -71,40 +124,3 @@ for (i in seq_along(mzTab_files)) {
 }
 PSM <- set_names(psm, file_names_short) #names each file by file_names_short
 view(PSM[[1]])
-
-#Finding matching peptide sequences between files
-#Work further with PSMs linked to TMTs, the index is needed
-PSM_TMT <- readRDS("~/Desktop/mzTab/Stored files/PSMs linked to TMT intensities")
-view(PSM_TMT[[1]])
-    #Make an extra index column that includes the file_names_short
-new_col <- list()
-for (i in seq_along(PSM_TMT)) {
-    new_col[[i]] <- PSM_TMT[[i]] %>%
-    mutate(index_filename = paste0(index, " ",file_names_short[[i]]), .before = index)
-}
-view(new_col[[4]])
-    #Selecting the sequence column + index_filename
-seq <- list()
-for (i in seq_along(new_col)) {
-    seq[[i]] <- select(new_col[[i]], index_filename, sequence)
-}
-(all_seq <- bind_rows(seq) %>% arrange(sequence, index_filename))
-view(all_seq) #all rows arrenged by sequence
-Sum_all_seq <- all_seq %>% add_column(count = rep(1, nrow(all_seq))) %>% 
-group_by(sequence) %>%
-summarise(Count = sum(count))
-view(Sum_all_seq) #sequence summarized, with a counter
-view(Sum_all_seq %>% arrange(sum)) #most frequent sequence at top
-    #Selecting the sequence_no_mod column + index_filename
-seq_no_mod <- list()
-for (i in seq_along(new_col)) {
-    seq_no_mod[[i]] <- select(new_col[[i]], index_filename, sequence_no_mod)
-}
-(all_seq_no_mod <- bind_rows(seq_no_mod) %>% arrange(sequence_no_mod, index_filename))
-view(all_seq_no_mod) #all rows arrenged by sequence_no_mod
-Sum_all_seq_no_mod <- all_seq_no_mod %>%
-add_column(count = rep(1, nrow(all_seq_no_mod))) %>%
-group_by(sequence_no_mod) %>%
-summarise(Count = sum(count))
-view(Sum_all_seq_no_mod) #sequence_no_mod summarized, with a counter
-view(Sum_all_seq_no_mod %>% arrange(-Count))#most frequent sequence_no_mod at top
