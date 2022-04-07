43cfc416d1acdd5fa478fb38fdf3f3e260d1cca7
Jens-VandePerre <Jens.VandePerre@student.uantwerpen.be>
2022-04-07 11:43:34 +0200

    Ran on 23 mzTabs 7/4/22

--- Read mzTab.r
+++ Read mzTab.r
@@ -24,7 +24,7 @@ library("stringr")
 
 wd <- setwd("~/Desktop/Inputs/ALL_mzTab")
 getwd() 
-list.files(wd)
+list.files(wd) #all mzTabs as of now 
 
 #Create Function readMzTab
   #Read an mzTab tab separated file 
@@ -38,14 +38,13 @@ readMzTab <- function(filename) {
                                   sep="\t", na.strings="null", quote = "")
   mztab.table
 }
-(Test <- readMzTab("02CPTAC_COprospective_W_PNNL_20170123_B1S2_f10.mztab")) #The first file
+(Test <- readMzTab("02CPTAC_COprospective_W_PNNL_20170123_B1S2_f10.mztab")) #File for testing
 #Loop Reading in Files
   #File paths to direct the loop
-file_paths <- fs::dir_ls("~/Desktop/mzTab/Imported mzTab")
-file_paths 
+(file_paths <- fs::dir_ls("~/Desktop/Inputs/ALL_mzTab"))
     #Automate filename extraction
-file_names_wd <- list.files(wd) #names of files in wd
-file_names_short <- substring(file_names_wd, 39, 46) #Character 39 untill 46 are unique
+(file_name_long <- list.files(wd))
+(file_names_short <- substring(file_paths, 86, 93)) #Characters 86 untill 93 are uniqueue
   #Loop reading mzTabs
 mzTab <- list() #empty list
 for (i in seq_along(file_paths)) {
@@ -53,10 +52,9 @@ for (i in seq_along(file_paths)) {
 }
 mzTab_files <- set_names(mzTab, file_names_short) #names each file by file_names_short
 mzTab_files[[1]]
-
  #Save mzMLs to different location: TMT outputs
-saveRDS(mzTab_files, file = "~/Desktop/mzTab/Stored files/Test 6 mzTabs")
-Test_6_mzTabs <- readRDS(file = "~/Desktop/mzTab/Stored files/Test 6 mzTabs")
+saveRDS(mzTab_files, file = "~/Desktop/Outputs/mzTabs_imported/07_04_22_mzTabs")
+mzTabs_07_04_22 <- readRDS(file = "~/Desktop/Outputs/mzTabs_imported/07_04_22_mzTabs")
 
 ##################
 #Extract functions
@@ -68,8 +66,8 @@ extractMetadata_long <- function(mztab.table) {
 }
   #Loop
 MTD_long <- list() #empty list
-for (i in seq_along(mzTab_files)) {
-  MTD_long[[i]] <- extractMetadata_long(mzTab_files[[i]])
+for (i in seq_along(mzTabs_07_04_22)) {
+  MTD_long[[i]] <- extractMetadata_long(mzTabs_07_04_22[[i]])
 }
 mzTab_files_Metadata_long <- set_names(MTD_long, file_names_short) #names each file by file_names_short
 mzTab_files_Metadata_long
@@ -81,8 +79,8 @@ extractMetadata <- function(mztab.table) {
 }
   #Loop
 MTD <- list() #empty list
-for (i in seq_along(mzTab_files)) {
-  MTD[[i]] <- extractMetadata(mzTab_files[[i]])
+for (i in seq_along(mzTabs_07_04_22)) {
+  MTD[[i]] <- extractMetadata(mzTabs_07_04_22[[i]])
 }
 mzTab_files_Metadata <- set_names(MTD, file_names_short) #names each file by file_names_short
 mzTab_files_Metadata
@@ -95,16 +93,11 @@ extractFeaturesPSM <- function(mztab.table) {
 }
   #Loop for all files
 PSM <- list() #empty list
-for (i in seq_along(mzTab_files)) {
-  PSM[[i]] <- extractFeaturesPSM(mzTab_files[[i]])
+for (i in seq_along(mzTabs_07_04_22)) {
+  PSM[[i]] <- extractFeaturesPSM(mzTabs_07_04_22[[i]])
 }
 mzTab_files_PSM <- set_names(PSM, file_names_short) #names each file by file_names_short
-mzTab_files_PSM
 view(mzTab_files_PSM[[1]])
-
-nrow(mzTab_files_PSM[[4]])
-n_distinct(mzTab_files_PSM[[4]])
-nrow(unique(mzTab_files_PSM[[4]]))
 
 #Determine identification percentage
   #Row count for mzTab_PSM = amount of identified peptides
@@ -117,17 +110,14 @@ for (i in seq_along(mzTab_files_PSM)) {
 Identified_peptides <- set_names(nrow_PSM, file_names_short) #names each file by file_names_short
 Identified_peptides
   #Row count original
-TMT_Matched_mzML_6 <- readRDS(file = "~/Desktop/mzTab/Stored files/6 matched mzMLS")
-n_distinct(TMT_Matched_mzML_6[[1]]) #count unique rows
-
+TMT_Intensities_06_04_22 <- readRDS(file = "~/Desktop/Outputs/TMTs/06.04.22_TMT")
     #Loop counting spectra
 nrow_TMT <- list() #empty list
-for (i in seq_along(TMT_Matched_mzML_6)) { #Only for the first 6 spectra, 
-  nrow_TMT[[i]] <- n_distinct(TMT_Matched_mzML_6[[i]])
+for (i in seq_along(TMT_Intensities_06_04_22)) { #Only for the first 6 spectra, 
+  nrow_TMT[[i]] <- n_distinct(TMT_Intensities_06_04_22[[i]])
 }
 Spectral_count <- set_names(nrow_TMT, file_names_short) #names each file by file_names_short
 Spectral_count
-
   #Loop percentage calculation
 perc <- list() #empty list
 for (i in seq_along(Identified_peptides)) { #Only for the first 6 spectra, 
@@ -135,7 +125,6 @@ for (i in seq_along(Identified_peptides)) { #Only for the first 6 spectra,
 }
 Identification_percentage <- set_names(perc, file_names_short) #names each file by file_names_short
 Identification_percentage
-
   #Making tibble of Identification_percentage, for plot
 df_perc <- matrix(unlist(Identification_percentage), nrow=length(Identification_percentage), byrow=TRUE)
 tbl_Identification_percentage <- tibble(File_Name=file_names_short , Identification_Percentage=df_perc)
@@ -145,13 +134,13 @@ p_perc <- ggplot(tbl_Identification_percentage, aes(x= File_Name , y= Identifica
   geom_col() +
   labs(x="File Name", y="Percentage Identified Spectra (%)", title="Identification Percentage" , 
         subtitle="Percentage of identified peptides for each file with spectra") +
-  geom_text(aes(label=round(df_perc, digits = 4)), 
-               position=position_dodge(width=0.9), vjust=-0.25, size = 3) +
+  geom_text(aes(label=round(df_perc, digits = 1)), 
+               position=position_dodge(width=0.9), vjust=-0.25, size = 2.5) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size = 10), axis.text.y = element_text(size = 10),
             plot.title = element_text(size = 18))
   #Print p_perc
 p_perc
-pdf(file = "~/Desktop/mzTab/Plots/Identification Percentage.pdf")
+pdf(file = "~/Desktop/Outputs/Plots/07_04_22_Identification_Percentage.pdf")
    p_perc
 dev.off()
 
@@ -170,8 +159,8 @@ tbl_id_perc <- cbind(tbl_name, tbl_perc)
 #Add new column sequence_no_mod behind the sequence column
         #Loop for all files
 psm <- list() #empty list
-for (i in seq_along(mzTab_files)) {
-  psm[[i]] <- extractFeaturesPSM(mzTab_files[[i]]) %>%
+for (i in seq_along(mzTabs_07_04_22)) {
+  psm[[i]] <- extractFeaturesPSM(mzTabs_07_04_22[[i]]) %>%
   as_tibble() %>% row_to_names(row_number = 1) %>%
   mutate(sequence_no_mod = trimws(str_remove_all(sequence, "n"))) %>% #remove n
   mutate(sequence_no_mod = trimws(str_remove_all(sequence_no_mod, "[0123456789]"))) %>% # remove numbers
@@ -181,5 +170,5 @@ for (i in seq_along(mzTab_files)) {
 PSM <- set_names(psm, file_names_short) #names each file by file_names_short
 view(PSM[[1]])
   #Store PSM files
-saveRDS(PSM, file = "~/Desktop/mzTab/Stored files/PSM column no modifications") #updated 1/4/22
-PSM_seq_no_mod <- readRDS(file = "~/Desktop/mzTab/Stored files/PSM column no modifications")
+saveRDS(PSM, file = "~/Desktop/Outputs/PSMs/07_04_22_PSMs") 
+PSM_07_04_22 <- readRDS(file = "~/Desktop/Outputs/PSMs/07_04_22_PSMs")
