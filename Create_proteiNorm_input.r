e8990aaeca68918f1308cc564fb94f4376207cd8
Jens-VandePerre <Jens.VandePerre@student.uantwerpen.be>
2022-05-03 17:05:05 +0200

    wip

--- Create_proteiNorm_input.r
+++ Create_proteiNorm_input.r
@@ -85,21 +85,50 @@ PSM_ready_for_matching <- set_names(ind_mzTab, file_names_short)
 PSM_ready_for_matching
 view(PSM_ready_for_matching[[1]]) #PSM column could be removed
 
+
+PSM_B1S1_f01 <- select(PSM_no_mod[[1]], PSM_ID) %>% 
+    mutate(index = trimws(str_remove_all(PSM_ID, "controllerType=0 "))) %>%
+    mutate(index = trimws(str_remove_all(index, "controllerNumber=1 "))) %>%
+    mutate(index = trimws(str_remove_all(index, "scan="))) %>%
+    select(index) %>%
+    cbind(PSM_no_mod[[1]]) %>% as_tibble
+
 #Match to TMTs
     #Load TMT saved
+#TMT_06.04.22 <- readRDS(file = "/Users/jensvandeperre/Desktop/Outputs/TMTs/06.04.22_TMT")
+#TMT_20.04.22 <- readRDS(file = "/Users/jensvandeperre/Desktop/Outputs/TMTs/20.04.22_TMT")
+#TMT_22.04.22 <- readRDS(file = "/Users/jensvandeperre/Desktop/Outputs/TMTs/22.04.22_TMT")
+#TMT_28.04.22 <- readRDS(file = "/Users/jensvandeperre/Desktop/Outputs/TMTs/28.04.22_TMT")
+
+TMT_part1_03.05.22 <- readRDS(file = "/Users/jensvandeperre/Desktop/Outputs/TMTs/03.05.22_TMT_part1")
+
+#TMT_part2_03.05.22 <- readRDS(file = "/Users/jensvandeperre/Desktop/Outputs/TMTs/03.05.22_TMT_part2")
+#TMT_part3_03.05.22 <- readRDS(file = "/Users/jensvandeperre/Desktop/Outputs/TMTs/03.05.22_TMT_part3")
+
 
     #Create index collumn
 ind_TMT1 <- list()
-for (i in seq_along(TMT_Intensities_22_04_22)) {
-  ind_TMT1[[i]] <- tibble(index=rownames(TMT_Intensities_22_04_22[[i]][, 0])) %>%
+for (i in seq_along(TMT_part1_03.05.22)) {
+  ind_TMT1[[i]] <- tibble(index=rownames(TMT_part1_03.05.22[[i]][, 0])) %>%
     mutate(index = trimws(str_remove_all(index, "F1.S"))) %>%
     mutate(index = trimws(str_remove_all(index, "^0"))) %>%
     mutate(index = trimws(str_remove_all(index, "^0"))) %>%
     mutate(index = trimws(str_remove_all(index, "^0"))) %>%
     mutate(index = trimws(str_remove_all(index, "^0"))) %>%
     select(index) %>%
-    cbind(TMT_Intensities_22_04_22[[i]]) %>% as_tibble
+    cbind(TMT_part1_03.05.22[[i]]) %>% as_tibble
 }
+
+TMT_B1S1_f01 <- tibble(index=rownames(TMT_part1_03.05.22[[1]][, 0])) %>%
+    mutate(index = trimws(str_remove_all(index, "F1.S"))) %>%
+    mutate(index = trimws(str_remove_all(index, "^0"))) %>%
+    mutate(index = trimws(str_remove_all(index, "^0"))) %>%
+    mutate(index = trimws(str_remove_all(index, "^0"))) %>%
+    mutate(index = trimws(str_remove_all(index, "^0"))) %>%
+    select(index) %>%
+    cbind(TMT_part1_03.05.22[[1]]) %>% as_tibble
+
+
 TMT_ready_for_machting <- set_names(ind_TMT1, file_names_short)
 TMT_ready_for_machting[[1]]
 view(TMT_ready_for_machting[[1]])
@@ -107,10 +136,17 @@ view(TMT_ready_for_machting[[1]])
 #Load protein info from PIA output
 test1 <- read.csv(file = '/Users/jensvandeperre/Desktop/Outputs/PIA_analysis/test_1.csv', header = TRUE, sep = "\t")
 test2 <- read.csv(file = '/Users/jensvandeperre/Desktop/Outputs/PIA_analysis/test_2.csv', header = TRUE, sep = "\t")
-test3 <- read.csv(file = '/Users/jensvandeperre/Desktop/Outputs/PIA_analysis/test_2.csv', header = TRUE, sep = "\t")
+test3 <- read.csv(file = '/Users/jensvandeperre/Desktop/Outputs/PIA_analysis/test_3.csv', header = TRUE, sep = "\t")
+test4_PSM <- read.csv(file = '/Users/jensvandeperre/Desktop/Outputs/PIA_analysis/PSM_exp_test_4.csv', header = TRUE, sep = "\t")
+test4_PRO <- read.csv(file = '/Users/jensvandeperre/Desktop/Outputs/PIA_analysis/PROT_exp_test_4.csv', header = TRUE, sep = "\t")
 view(test1)
 view(test2)
 view(test3)
+view(test4_PSM)
+view(test4_PRO)
+
+
+scan(text=str1, sep="_", what="", quiet=TRUE)[3]
 
 
 
@@ -130,3 +166,20 @@ for (i in seq_along(TMT_Intensities_22_04_22)) {
 TMT_ready_for_machting <- set_names(ind_TMT1, file_names_short)
 TMT_ready_for_machting[[1]]
 view(TMT_ready_for_machting[[1]])
+
+
+
+#Create Peptide.txt
+  #Leading rezor peptide
+  test4_PRO %>% filter(COLS_PROTEIN.Proteins.Score.Coverages.nrPeptides.nrPSM.nrSpectra.ClusterID.Description.Decoy.FDR.q.valu, str_detect("PROTEIN"))
+  #Gene names
+  #Reporter intensity corrected
+  #Reverse
+  #Potential contaminant
+  #id = just number
+  #Protein groups IDs
+
+
+#Create ProteinGroup.txt
+  #ID = leading rezor peptide 
+  #Reporter intensity corrected
