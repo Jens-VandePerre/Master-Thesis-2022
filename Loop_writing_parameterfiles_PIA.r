a4cd4d08dc47bc134c1edf086da261bb5b8fa647
Jens-VandePerre <Jens.VandePerre@student.uantwerpen.be>
2022-05-17 11:56:31 +0200

    Loop writing JSON parameterfiles for PIA 17/5/22

--- Loop_writing_parameterfiles_PIA.r
+++ Loop_writing_parameterfiles_PIA.r
@@ -1,13 +1,40 @@
+library(tidyverse)
+library(data.table)
+library("rpx")
+library("mzR")
+library("OrgMassSpecR")
+library("biomaRt")
+library("Hmisc")
+library("gplots")
+library("limma")
+library("isobar")
+library("devtools")
+library("MSnbase")
+library("Biobase")
+library("dplyr")
+library("tidyverse")
+library("fs")
+library("proxyC")
+library("sjlabelled")
+library("expss")
+library("labelled")
+library("patchwork")
+library("msdata")
+library("remotes")
+library("janitor")
+library("stringr")
+library("purrr")
+library("rlist")
+
 wd <- setwd("/Users/jensvandeperre/Desktop/Inputs/ALL_mzTab")
 getwd() 
 list.files(wd)
     #Automate filename extraction
 (file_name_long <- substring(list.files(wd), 1, 46))
-(file_paths <- fs::dir_ls("/Users/jensvandeperre/Desktop/Inputs/ALL_mzTab"))
 (file_names_short <- substring(file_name_long, 39, 46)) 
 
 #Select the WHOLE string!!!
-text_before <-
+text_before <- #from here
 '"considerModifications": false,
 "createPSMsets": true,
 "errorOnNoDecoys": false,
@@ -23,7 +50,7 @@ text_before <-
 "proteinFilters": [
     "nr_unique_peptides_per_protein_filter >= 2"
 ],
-"proteinExportFile": "/Users/jensvandeperre/Desktop/Outputs/PIA_analysis/PIA_output_'
+"proteinExportFile": "/Users/jensvandeperre/Desktop/Outputs/PIA_analysis/PIA_output_' #until here
 
 #Filename
 
@@ -39,7 +66,11 @@ for (i in 1:264) {
     parameter_tex[[i]] <- 
     paste(text_before, file_name_long[[i]], text_after, sep = "")
 }
-parameter_tex[[1]]
+view(parameter_tex[[1]])
+parameter_tex[[20]]
+parameter_tex[[264]]
 
 #Loop writing JSON files
-JSON <- list()
+for (i in 1:264) {
+   write(parameter_tex[[i]], file = paste("/Users/jensvandeperre/Desktop/Inputs/PIA_parameter_files/", file_name_long[[i]], ".json", sep=""))
+}
