0d3de99509061a9f509a09d94ed94d2f3d6db439
Jens-VandePerre <Jens.VandePerre@student.uantwerpen.be>
2022-05-17 16:30:39 +0200

    calculated PTM mass differences and stored as csv

--- Calculate_PTM_mass.r
+++ Calculate_PTM_mass.r
@@ -33,12 +33,8 @@ library("rlist")
 #mass_tolerance <- as.numeric(args[3])
 #output_path <- paste(bait, "_tol_py.csv", sep = "")
 
-bait <- name
-mass_tolerance <- 20
-output_path <- "/Users/jensvandeperre/Desktop/Inputs/PTM_mass_differences/PTM_mass_diff_"
-
 #   PSM file (TMTs are included, but not needed)
-PSM_TMT_all <- readRDS("~/Desktop/Outputs/PSM_TMT_linked/ALL_PSM_TMT_Linked")
+PSM_TMT_all <- readRDS("/Users/jensvandeperre/Desktop/Outputs/PSM_TMT_linked/ALL_PSM_TMT_Linked")
 view(PSM_TMT_all[[1]])
 str(PSM_TMT_all[[1]])
 
@@ -51,25 +47,53 @@ list.files(wd)
 (file_names_short <- substring(file_name_long, 39, 46))
 
 #   calculate the mass tolerance in Da for each psm
-mztab_mtol <- PSM_TMT_all[[1]] %>% as_tibble %>%
-    select(sequence, PSM_ID, exp_mass_to_charge, calc_mass_to_charge, charge) %>%
+    #Loop
+mass_tolerance_20 <- 20
+mztab_mtol_20 <- list()
+for (i in 1:264) {
+    mztab_mtol_20[[i]] <- PSM_TMT_all[[i]] %>% 
+    as_tibble %>%
+    dplyr::select(sequence, PSM_ID, exp_mass_to_charge, calc_mass_to_charge, charge) %>%
     mutate(exp_mass_to_charge = as.numeric(exp_mass_to_charge)) %>%
     mutate(calc_mass_to_charge = as.numeric(calc_mass_to_charge)) %>%
     mutate(charge = as.numeric(charge)) %>%
     mutate(mass_diff = (exp_mass_to_charge - calc_mass_to_charge)*charge) %>%
     mutate(exp_mass = exp_mass_to_charge * charge) %>%
-    mutate(mass_tol = mass_tolerance * exp_mass * 10**-6) %>%   #something goes wrong here
+    mutate(mass_tol = mass_tolerance_20 * exp_mass * 10**-6) %>%
     mutate(mass_tol_pos = mass_diff + mass_tol) %>%
     mutate(mass_tol_neg = mass_diff - mass_tol) %>%
-    filter(!(mass_tol_neg < 0 & mass_tol_pos > 0)) %>%      # filter out unmodified psms
-    mutate(file_name_long = file_name_long) %>%
+    filter(!(mass_tol_neg < 0 & mass_tol_pos > 0)) %>% #filter out unmodified psms
     arrange(mass_tol_pos)
+}
+view(mztab_mtol_20[[1]])
 
-view(mztab_mtol)
-str(mztab_mtol)
+mass_tolerance_10 <- 10
+mztab_mtol_10 <- list()
+for (i in 1:264) {
+    mztab_mtol_10[[i]] <- PSM_TMT_all[[i]] %>% 
+    as_tibble %>%
+    dplyr::select(sequence, PSM_ID, exp_mass_to_charge, calc_mass_to_charge, charge) %>%
+    mutate(exp_mass_to_charge = as.numeric(exp_mass_to_charge)) %>%
+    mutate(calc_mass_to_charge = as.numeric(calc_mass_to_charge)) %>%
+    mutate(charge = as.numeric(charge)) %>%
+    mutate(mass_diff = (exp_mass_to_charge - calc_mass_to_charge)*charge) %>%
+    mutate(exp_mass = exp_mass_to_charge * charge) %>%
+    mutate(mass_tol = mass_tolerance_10 * exp_mass * 10**-6) %>%
+    mutate(mass_tol_pos = mass_diff + mass_tol) %>%
+    mutate(mass_tol_neg = mass_diff - mass_tol) %>%
+    filter(!(mass_tol_neg < 0 & mass_tol_pos > 0)) %>% #filter out unmodified psms
+    arrange(mass_tol_pos)
+}
+view(mztab_mtol_10[[1]])
 
 #   save to file for python
+    # mass_tolerance = 20
 for (i in 1:264) {
-fwrite(mztab_mtol[[i]], file = paste(output_path, file_name_long[[i]], ".csv", sep = ""), na = "NA", append = FALSE, col.names = TRUE)
-
+    fwrite(mztab_mtol_20[[i]], file = paste("/Users/jensvandeperre/Desktop/Inputs/PTM_mass_differences/Mass_tolerance_20/"
+    , file_name_long[[i]], ".csv", sep = ""), na = "NA", append = FALSE, col.names = TRUE)
+}
+    # mass_tolerance = 10
+for (i in 1:264) {
+    fwrite(mztab_mtol_10[[i]], file = paste("/Users/jensvandeperre/Desktop/Inputs/PTM_mass_differences/Mass_tolerance_10/"
+    , file_name_long[[i]], ".csv", sep = ""), na = "NA", append = FALSE, col.names = TRUE)
 }
