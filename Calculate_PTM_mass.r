d5685be48b63190e5a6655d35c127906d38f01ca
Jens-VandePerre <Jens.VandePerre@student.uantwerpen.be>
2022-05-17 14:53:38 +0200

    wip

--- Calculate_PTM_mass.r
+++ Calculate_PTM_mass.r
@@ -26,25 +26,29 @@ library("stringr")
 library("purrr")
 library("rlist")
 
-
-#   command line
+#   command line (not needed for running in R)
 #args <- commandArgs(trailingOnly = TRUE)
 #csv <- args[1]
 #bait <- args[2]
 #mass_tolerance <- as.numeric(args[3])
 #output_path <- paste(bait, "_tol_py.csv", sep = "")
 
-args <- TRUE
-csv <- csv
 bait <- name
 mass_tolerance <- 20
-output_path <- outputpath
+output_path <- "/Users/jensvandeperre/Desktop/Inputs/PTM_mass_differences/PTM_mass_diff_"
 
-#   mztab file
-mztab = fread(file=csv, sep = '\t')
+#   PSM file (TMTs are included, but not needed)
 PSM_TMT_all <- readRDS("~/Desktop/Outputs/PSM_TMT_linked/ALL_PSM_TMT_Linked")
 view(PSM_TMT_all[[1]])
 str(PSM_TMT_all[[1]])
+
+#File naming 
+wd <- setwd("/Users/jensvandeperre/Desktop/Inputs/ALL_mzTab")
+getwd() 
+list.files(wd)
+    #Automate filename extraction
+(file_name_long <- substring(list.files(wd), 1, 46))
+(file_names_short <- substring(file_name_long, 39, 46))
 
 #   calculate the mass tolerance in Da for each psm
 mztab_mtol <- PSM_TMT_all[[1]] %>% as_tibble %>%
@@ -58,11 +62,14 @@ mztab_mtol <- PSM_TMT_all[[1]] %>% as_tibble %>%
     mutate(mass_tol_pos = mass_diff + mass_tol) %>%
     mutate(mass_tol_neg = mass_diff - mass_tol) %>%
     filter(!(mass_tol_neg < 0 & mass_tol_pos > 0)) %>%      # filter out unmodified psms
-    mutate(bait = bait) %>%
+    mutate(file_name_long = file_name_long) %>%
     arrange(mass_tol_pos)
 
 view(mztab_mtol)
 str(mztab_mtol)
 
 #   save to file for python
-fwrite(mztab_mtol, output_path, na = "NA", append = FALSE, col.names = TRUE)
+for (i in 1:264) {
+fwrite(mztab_mtol[[i]], file = paste(output_path, file_name_long[[i]], ".csv", sep = ""), na = "NA", append = FALSE, col.names = TRUE)
+
+}
