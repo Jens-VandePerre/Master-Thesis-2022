cbf2d1d6938d1105508f787def0d4026540d5314
Jens-VandePerre <Jens.VandePerre@student.uantwerpen.be>
2022-05-18 16:13:40 +0200

    output with alle collumns

--- Calculate_PTM_mass.r
+++ Calculate_PTM_mass.r
@@ -1,5 +1,4 @@
-library(tidyverse)
-library(data.table)
+library("data.table")
 library("rpx")
 library("mzR")
 library("OrgMassSpecR")
@@ -95,5 +94,58 @@ for (i in 1:264) {
     # mass_tolerance = 10
 for (i in 1:264) {
     fwrite(mztab_mtol_10[[i]], file = paste("/Users/jensvandeperre/Desktop/Inputs/PTM_mass_differences/Mass_tolerance_10/"
+    , file_name_long[[i]], ".csv", sep = ""), na = "NA", append = FALSE, col.names = TRUE)
+}
+
+#Leave all columns in
+    #Loop
+mass_tolerance_20 <- 20
+PSM_20 <- list()
+for (i in 1:264) {
+    PSM_20[[i]] <- PSM_TMT_all[[i]] %>% 
+    as_tibble %>%
+    mutate(exp_mass_to_charge = as.numeric(exp_mass_to_charge)) %>%
+    mutate(calc_mass_to_charge = as.numeric(calc_mass_to_charge)) %>%
+    mutate(charge = as.numeric(charge)) %>%
+    mutate(mass_diff = (exp_mass_to_charge - calc_mass_to_charge)*charge) %>%
+    mutate(exp_mass = exp_mass_to_charge * charge) %>%
+    mutate(mass_tol = mass_tolerance_20 * exp_mass * 10**-6) %>%
+    mutate(mass_tol_pos = mass_diff + mass_tol) %>%
+    mutate(mass_tol_neg = mass_diff - mass_tol) %>%
+    filter(!(mass_tol_neg < 0 & mass_tol_pos > 0)) %>% #filter out unmodified psms
+    arrange(mass_tol_pos)
+}
+view(PSM_20[[1]])
+length(PSM_20)
+
+mass_tolerance_10 <- 10
+PSM_10 <- list()
+for (i in 1:264) {
+    PSM_10[[i]] <- PSM_TMT_all[[i]] %>% 
+    as_tibble %>%
+    mutate(exp_mass_to_charge = as.numeric(exp_mass_to_charge)) %>%
+    mutate(calc_mass_to_charge = as.numeric(calc_mass_to_charge)) %>%
+    mutate(charge = as.numeric(charge)) %>%
+    mutate(mass_diff = (exp_mass_to_charge - calc_mass_to_charge)*charge) %>%
+    mutate(exp_mass = exp_mass_to_charge * charge) %>%
+    mutate(mass_tol = mass_tolerance_10 * exp_mass * 10**-6) %>%
+    mutate(mass_tol_pos = mass_diff + mass_tol) %>%
+    mutate(mass_tol_neg = mass_diff - mass_tol) %>%
+    filter(!(mass_tol_neg < 0 & mass_tol_pos > 0)) %>% #filter out unmodified psms
+    arrange(mass_tol_pos)
+}
+view(PSMl_10[[1]])
+length(PSM_10)
+
+
+# Save files for Relative Quantification
+    # mass_tolerance = 20
+for (i in 1:264) {
+    fwrite(PSM_20[[i]], file = paste("/Users/jensvandeperre/Desktop/Inputs/PSM_TMT_mass_diff/Mass_tolerance_20/"
+    , file_name_long[[i]], ".csv", sep = ""), na = "NA", append = FALSE, col.names = TRUE)
+}
+    # mass_tolerance = 10
+for (i in 1:264) {
+    fwrite(PSMl_10[[i]], file = paste("/Users/jensvandeperre/Desktop/Inputs/PSM_TMT_mass_diff/Mass_tolerance_10/"
     , file_name_long[[i]], ".csv", sep = ""), na = "NA", append = FALSE, col.names = TRUE)
 }
